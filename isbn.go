c38f2642fbd56b3fe494ae1725c74b4d1d442b0e
BooleanCat <thgodkin@gmail.com>
2019-12-26 11:37:26 +0000

    implement RegistrationGroup

--- isbn.go
+++ isbn.go
@@ -18,13 +18,25 @@ func NewISBN13(i uint64) (ISBN13, error) {
 		return 0, errors.New("incorrect check digit")
 	}
 
-	return ISBN13(i), nil
+	isbn := ISBN13(i)
+	if _, err := isbn.registrationGroup(); err != nil {
+		return 0, err
+	}
+
+	return isbn, nil
 }
 
 // GS1 returns the three digit number issued by the GS1 Global Office to the
 // Registration Authority.
 func (isbn ISBN13) GS1() uint64 {
 	return uint64(isbn / 10e9)
+}
+
+// RegistrationGroup identifies to which national, geographic, language or
+// otherwise the ISBN belongs to.
+func (isbn ISBN13) RegistrationGroup() uint64 {
+	group, _ := isbn.registrationGroup()
+	return group
 }
 
 // CheckDigit returns the final digit of the ISBN13, a best effort validity
@@ -50,4 +62,106 @@ func calculateCheckDigit(i uint64) uint64 {
 	}
 
 	return sum % 10
+}
+
+func (isbn ISBN13) registrationGroup() (uint64, error) {
+	if isbn.GS1() == 978 {
+		return isbn.registrationGroupGS1978()
+	}
+
+	return isbn.registrationGroupGS1979()
+}
+func (isbn ISBN13) registrationGroupGS1978() (uint64, error) {
+	for _, n := range registrationGroupBound978 {
+		digits := uint64(isbn) / n.divisor % n.mod
+		for _, b := range n.bounds {
+			if uint32(digits) >= b.lower && uint32(digits) <= b.upper {
+				return digits, nil
+			}
+		}
+	}
+
+	return 0, errors.New("registration group not defined")
+}
+
+func (isbn ISBN13) registrationGroupGS1979() (uint64, error) {
+	for _, n := range registrationGroupBound979 {
+		digits := uint64(isbn) / n.divisor % n.mod
+		for _, b := range n.bounds {
+			if uint32(digits) >= b.lower && uint32(digits) <= b.upper {
+				return digits, nil
+			}
+		}
+	}
+
+	return 0, errors.New("registration group not defined")
+}
+
+type bound struct {
+	lower uint32
+	upper uint32
+}
+
+type bounds struct {
+	bounds  []bound
+	divisor uint64
+	mod     uint64
+}
+
+var registrationGroupBound978 = map[uint8]bounds{
+	1: {
+		divisor: 10e8,
+		mod:     10,
+		bounds: []bound{
+			{lower: 0, upper: 5},
+			{lower: 7, upper: 7},
+		},
+	},
+	2: {
+		divisor: 10e7,
+		mod:     100,
+		bounds: []bound{
+			{lower: 80, upper: 94},
+			{lower: 65, upper: 65},
+		},
+	},
+	3: {
+		divisor: 10e6,
+		mod:     1000,
+		bounds: []bound{
+			{lower: 600, upper: 649},
+			{lower: 950, upper: 989},
+		},
+	},
+	4: {
+		divisor: 10e5,
+		mod:     10000,
+		bounds: []bound{
+			{lower: 9900, upper: 9989},
+		},
+	},
+	5: {
+		divisor: 10e4,
+		mod:     100000,
+		bounds: []bound{
+			{lower: 99900, upper: 99999},
+		},
+	},
+}
+
+var registrationGroupBound979 = map[uint8]bounds{
+	1: {
+		divisor: 10e8,
+		mod:     10,
+		bounds: []bound{
+			{lower: 8, upper: 8},
+		},
+	},
+	2: {
+		divisor: 10e7,
+		mod:     100,
+		bounds: []bound{
+			{lower: 10, upper: 12},
+		},
+	},
 }
--- isbn_test.go
+++ isbn_test.go
@@ -23,6 +23,11 @@ func TestNewISBN13_TooLong(t *testing.T) {
 	assert.ErrorMatches(t, err, "not a 13-digit number")
 }
 
+func TestNewISBN13_RegistrationGroupUndefined(t *testing.T) {
+	_, err := isbn.NewISBN13(9786675094184)
+	assert.ErrorMatches(t, err, "registration group not defined")
+}
+
 func TestNewISBN13_IncorrectCheckDigit(t *testing.T) {
 	_, err := isbn.NewISBN13(9780575094184)
 	assert.ErrorMatches(t, err, "incorrect check digit")
@@ -30,6 +35,36 @@ func TestNewISBN13_IncorrectCheckDigit(t *testing.T) {
 
 func TestISBN13_GS1(t *testing.T) {
 	assert.Equal(t, isbn.ISBN13(9780575094185).GS1(), uint64(978))
+}
+
+func TestISBN13_RegistrationGroup(t *testing.T) {
+	tests := map[string]struct {
+		isbn isbn.ISBN13
+		want uint64
+	}{
+		"GS1 978 single digit lower bound": {isbn: 9780575094185, want: 0},
+		"GS1 978 single digit upper bound": {isbn: 9785575094180, want: 5},
+		"GS1 978 single digit 7":           {isbn: 9787575094184, want: 7},
+		"GS1 978 two digit 65":             {isbn: 9786575094187, want: 65},
+		"GS1 978 two digit lower bound":    {isbn: 9788075094186, want: 80},
+		"GS1 978 two digit upper bound":    {isbn: 9789475094189, want: 94},
+		"GS1 978 three digit lower bound":  {isbn: 9786005094183, want: 600},
+		"GS1 978 three digit upper bound":  {isbn: 9786495094182, want: 649},
+		"GS1 978 four digit lower bound":   {isbn: 9789900094180, want: 9900},
+		"GS1 978 four digit upper bound":   {isbn: 9789989094187, want: 9989},
+		"GS1 978 five digit lower bound":   {isbn: 9789990094183, want: 99900},
+		"GS1 978 five digit upper bound":   {isbn: 9789999994187, want: 99999},
+
+		"GS1 979 single digit 8":        {isbn: 9798495094185, want: 8},
+		"GS1 979 two digit lower bound": {isbn: 9791095094185, want: 10},
+		"GS1 979 two digit upper bound": {isbn: 9791295094185, want: 12},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, isbn.ISBN13(tc.isbn).RegistrationGroup(), tc.want)
+		})
+	}
 }
 
 func TestISBN13_CheckDigit(t *testing.T) {
